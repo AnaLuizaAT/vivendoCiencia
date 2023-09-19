a360ed9ce8071b3c23a751d4e9b86ebd892189a8
Ana Luiza Araujo <aluiza.araujot@gmail.com>
2023-09-19 00:38:39 -0300

    Changes

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener("scroll", onScroll);
 
-onScroll()
+onScroll();
 function onScroll() {
-    showBackToTopButtonOnScroll()
-    activateMenuAtCurrentSection(home)
-    activateMenuAtCurrentSection(services)
-    activateMenuAtCurrentSection(about)
-    activateMenuAtCurrentSection(contact)
+  showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
 }
 
 function activateMenuAtCurrentSection(section) {
-    const targetLine = scrollY + innerHeight / 2
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
-    const sectionEndsAt = sectionTop + sectionHeight
-    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
-    const sectionBoundaries =
-        sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
-    const sectionId = section.getAttribute('id')
-    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
-    menuElement.classList.remove('active')
-    if (sectionBoundaries) {
-        menuElement.classList.add('active')
-    }
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showBackToTopButtonOnScroll() {
-    if (scrollY > 550) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
 }
 
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+  document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+  document.body.classList.remove("menu-expanded");
 }
 
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700
+  origin: "top",
+  distance: "30px",
+  duration: 700,
 }).reveal(
-    `#home, 
+  `#home, 
     #home img, 
     #home .stats, 
     #services,
@@ -56,69 +56,75 @@ ScrollReveal({
     #about,
     #about header
     #about .content`
-)
+);
 
-const cards = document.querySelectorAll('.card')
-const cardsContainer = document.querySelector('.cards')
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        // verifica se já tá selecionado e remove a classe
-        if (card.classList.contains('selected')) {
-            card.classList.remove('selected')
-            card.style.backgroundColor = '#ffffff'
-            card.style.border = '1px solid hsl(calc(var(--hue) - 22), 23%, 89%)'
-        } else {
-            // se não tiver selecionado, adiciona a classe
-            card.classList.add('selected')
-            card.style.backgroundColor = '#56f86c'
-            card.style.border = '1px solid #008b00'
-        }
-    })
-})
+const cards = document.querySelectorAll(".card");
+const cardsContainer = document.querySelector(".cards");
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    // verifica se já tá selecionado e remove a classe
+    if (card.classList.contains("selected")) {
+      card.classList.remove("selected");
+      card.style.backgroundColor = "#ffffff";
+      card.style.border = "1px solid hsl(calc(var(--hue) - 22), 23%, 89%)";
+    } else {
+      // se não tiver selecionado, adiciona a classe
+      card.classList.add("selected");
+      card.style.backgroundColor = "#56f86c";
+      card.style.border = "1px solid #008b00";
+    }
+  });
+});
 
 function updateSelectedCardsInput() {
-    document.getElementById('atividades').value = [
-        ...document.querySelectorAll('.card.selected h3')
-    ].map(e => e.innerText).join(',' + "\n")
+  document.getElementById("atividades").value = [
+    ...document.querySelectorAll(".card.selected h3"),
+  ]
+    .map((e) => e.innerText)
+    .join("," + "\n");
 }
 
 function getSelectedCardsValue() {
-    return [...document.querySelectorAll('.card.selected')]
-        .map(e => parseFloat(e.dataset.value))
-        .reduce((total, value) => total + value, 0)
+  return [...document.querySelectorAll(".card.selected")]
+    .map((e) => parseFloat(e.dataset.value))
+    .reduce((total, value) => total + value, 0);
 }
 
-var modal = document.getElementById('myModal')
-var buyBtn = document.getElementById('buy-btn')
-var btn = document.getElementsByClassName('open-modal-btn')[0]
-var span = document.getElementsByClassName('close')[0]
+var modal = document.getElementById("myModal");
+var buyBtn = document.getElementById("buy-btn");
+var btn = document.getElementsByClassName("open-modal-btn")[0];
+var span = document.getElementsByClassName("close")[0];
 
 const openModal = () => {
-    document.getElementById('pix-value').innerText = getSelectedCardsValue().toLocaleString(undefined, { style: 'currency', currency: 'BRL' })
-    modal.style.display = 'block'
-}
+  document.getElementById("pix-value").innerText =
+    getSelectedCardsValue().toLocaleString(undefined, {
+      style: "currency",
+      currency: "BRL",
+    });
+  modal.style.display = "block";
+};
 
-btn.onclick = openModal
-buyBtn.onclick = openModal
+btn.onclick = openModal;
+buyBtn.onclick = openModal;
 
 span.onclick = function () {
-    modal.style.display = 'none'
-}
+  modal.style.display = "none";
+};
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = 'none'
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-const fileInput = document.getElementById('comprovante')
-const fileNameSpan = document.getElementById('file-name')
+const fileInput = document.getElementById("comprovante");
+const fileNameSpan = document.getElementById("file-name");
 
-fileInput.addEventListener('change', function () {
-    if (fileInput.files.length > 0) {
-        const fileName = fileInput.files[0].name
-        fileNameSpan.textContent = fileName
-    } else {
-        fileNameSpan.textContent = ''
-    }
-})
+fileInput.addEventListener("change", function () {
+  if (fileInput.files.length > 0) {
+    const fileName = fileInput.files[0].name;
+    fileNameSpan.textContent = fileName;
+  } else {
+    fileNameSpan.textContent = "";
+  }
+});
